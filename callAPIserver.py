2cd0511bc2c741914d27a9346c43249a2ddcc920
christoph schwalbe <eracsjob@googlemail.com>
2020-12-30 20:36:24 +0100

    play around with 2nd API

---
+++ callAPIserver.py
@@ -0,0 +1,61 @@
+import sys
+#import pygame
+import time
+#import string
+#import logging
+import json
+#import urllib
+import requests
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+datadict = {}
+
+
+def callServer( ):
+    """
+    returns dictionary with response from weather API Server
+    if response HTTP Status != 200 returns None
+    """
+
+    url = 'https://api.forecast.io/forecast/500e8abf656226b5076cd1886f87f8b2/51.0687216,13.6849743?units=si'
+    params = {}
+    # params = dict(foo='', foo2='')
+    resp = requests.get(url=url, params=params)
+    if (resp.status_code == 200):
+        data = resp.json()
+        pp.pprint(data)
+        return data
+    return None
+
+
+def test():
+#while True:
+    retVal = callServer()
+    #print(retVal)
+
+    # extract current data for today
+    today = ""
+    windSpeed = retVal['currently']['windSpeed']
+    currWind = "{} km/h ".format(windSpeed)
+    currTemp = str( retVal['currently']['temperature'] ) + u' \N{DEGREE SIGN}' + "C"
+    currPress = str( retVal['currently']['pressure'] ) + " mb"
+    uv = "Ozone {}".format(retVal['currently']['ozone'])
+    humid = "Hum {}%".format(retVal['currently']['humidity'])
+    currTempFeeling = "(" + str( retVal['currently']['apparentTemperature'] ) + u' \N{DEGREE SIGN}' + "C)"
+    todayDesc = "It is " + str( retVal['currently']['summary'].lower() ) + " today."
+
+    print(today)
+    print(windSpeed)
+    print(currWind)
+    print(currTemp)
+    print(currPress)
+    print(uv)
+    print(humid)
+    print(currTempFeeling)
+    print(todayDesc)
+    
+    #time.sleep(30)
+    sys.exit(0)
+
+test()
