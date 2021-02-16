85620635e32f7067ae08af186d1b56875e9b98e5
christoph schwalbe <eracsjob@googlemail.com>
2021-02-17 00:42:49 +0100

    some misc cleaning

--- obsolete/testCode.py
+++ obsolete/testCode.py
@@ -4,10 +4,10 @@ import json
 
 # Formated Printer
 pp = pprint.PrettyPrinter(indent=4)
- 
+
 # Server call
 cottbus = pywapi.get_weather_from_weather_com('GMXX0171')
- 
+
 # Print to console
 pp.pprint(cottbus)
 
